18176c8d653bc7de572f22dbfbeb30fe5a10f4d2
Steven Miklovic <smiklov1@jh.edu>
2026-07-22 15:08:58 -0400

    Fix: alb delete protection

--- infra/modules/mcp-shared/alb.tf
+++ infra/modules/mcp-shared/alb.tf
@@ -59,7 +59,7 @@ resource "aws_lb" "mcp" {
   security_groups            = [aws_security_group.alb.id]
   subnets                    = var.public_subnet_ids
   drop_invalid_header_fields = true
-  enable_deletion_protection = true
+  enable_deletion_protection = var.enable_deletion_protection
   idle_timeout               = 65
 
   tags = local.common_tags
